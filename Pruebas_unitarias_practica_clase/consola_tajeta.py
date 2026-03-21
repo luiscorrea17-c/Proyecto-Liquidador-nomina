ead380a45dbea81179f45619241e54961fbb9882
Sebastian Rendon <srendon521@gmail.com>
2026-03-20 19:19:44 -0500

    Se elimino la carpeta innecesaria

--- Pruebas_unitarias_practica_clase/consola_tajeta.py
+++
@@ -1,9 +0,0 @@
-import logica_tarjeta
-
-compra= input("ingrese el valor de la compra: ")
-interes = input("ingrese el valor del interés mensual en porcentaje: ")
-plazo = input("ingrese el número de meses para pagar la compra: ")
-
-cuota = logica_tarjeta.calcular_cuota(compra, interes, plazo)
-
-print( cuota)
